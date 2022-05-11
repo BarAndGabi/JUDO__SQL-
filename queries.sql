ead81dc2ffcf3c6f73a7a99e02fa6d0b21838a47
GabrieLempert <81615014+GabrieLempert@users.noreply.github.com>
2022-05-11 18:00:45 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
-use judo;
+
 -- get all male judokas from category heavy and old order by win count
 select  teammates.id,name,team,weight,gender,`coach id`,age,category,`is injured`,wins,`game played`
 from teammates 
@@ -18,9 +18,9 @@ from categorys,judokas
 where judokas.category=categorys.id
 group by categorys.id;
 
--- list of shlomi(204) battles
+-- list of shuky(207) battles
 
-select * from battles where judge_id=204;
+select * from battles where judge_id=207;
 
 
 -- list of judokas fouls
@@ -29,7 +29,7 @@ from teammates,fouls
 where fouls.judokas_id=teammates.id
 group by teammates.id;
 
--- list of judge 204 most judged category
+-- list of judge 207 most judged category
 
 SELECT judge.name,battles.category,count(battles.category) as battles_count
 FROM judge
@@ -42,15 +42,21 @@ ORDER BY  count(battles.category) DESC LIMIT 1;
 
 SELECT teammates.name,category,count(`battle results`.winner) as result
 FROM judokas,`battle results`,teammates
-WHERE judokas.id=`battle results`.winner and  judokas.id=teammates.id
+WHERE judokas.id=`battle results`.winner and  judokas.id=teammates.id 
 GROUP BY judokas.id,judokas.category
-ORDER BY  result DESC 
-LIMIT 8;
+ORDER BY  result DESC,category DESC;
+drop function give_category;
+CREATE FUNCTION give_category(category_id varchar(3))
+RETURNS INT  deterministic
+RETURN (SELECT judokas.id
+FROM judokas,`battle results`,teammates
+WHERE judokas.id=`battle results`.winner and  judokas.id=teammates.id and category=category_id
+GROUP BY judokas.id,judokas.category
+ORDER BY  count(`battle results`.winner) DESC,category DESC LIMIT 1);
+ 
 
--- get list of all judokas precentage of victory
-
-select  teammates.name,judokas.wins/judokas.`game played` as win_ratio
-from teammates,judokas
-where teammates.id = judokas.id;
-
+SELECT teammates.name,categorys.id,count(`battle results`.winner) as 'Win Count'
+FROM categorys,teammates,`battle results`
+WHERE teammates.id=give_category(categorys.id) and give_category(categorys.id)=`battle results`.winner
+GROUP BY teammates.id,categorys.id;
 
